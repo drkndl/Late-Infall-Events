12f98b4f1c2c43249cd00f4327d0dfd01d937efb
tr323@uni-heidelberg.de <hd_tr323@login02.cluster>
2025-10-14 11:28:51 +0200

    iras04125_it450 imgs

--- time_evol.py
+++ time_evol.py
@@ -24,13 +24,14 @@ stoky = 3.156e7 * 1e3     # 1 kyr in sec
 def main():
 
 
-    folder = Path("../cloud_disk_it450_retro_rotX45/")          # Folder with the FARGO output files
-    fig_imgs = Path("cloud_disk_it450_retro_rotX45/imgs/")      # Folder to save images    
+    # folder = Path("../cloud_disk_it450_retro_rotX45/")                        # Folder with the FARGO output files
+    folder = Path("../fargo3d/outputs/iras04125_it450/")          # Folder with the FARGO output files (Binac2)
+    fig_imgs = Path("iras04125_it450/imgs/")                      # Folder to save images    
     iter_total = 450                                     # FARGO snapshot
 
-    first_it = 1
-    iter_check = np.arange(first_it, iter_total+1, 75)                       # Some iterations to plot
-    sim_name = str(folder).split('/')[1]                                     # Simulation name (for plot labels)
+    first_it = 150
+    iter_check = np.arange(first_it, iter_total+1, 50)                       # Some iterations to plot
+    sim_name = str(fig_imgs).split('/')[0]                                     # Simulation name (for plot labels)
     dt_years = calc_simtime(np.asarray(range(first_it, iter_total+1)))       # Convert iterations to kyrs
     dtkyrs_check = calc_simtime(iter_check)                                  # Convert iterations into kyrs
 
@@ -94,8 +95,8 @@ def main():
 
         # Note 1: I am using centered densities to isolate the warp to match the indices corresponding to the warp with the angular momenta indices
         # Note 2: The warp_ids itself is a 3D Boolean array, but when applied to another array such as x[warp_ids], the latter array becomes 1D
-        warp_thresh = -17   # log of density threshold for which we can see the warp in the primary
-        warp_buffer = 300     # Isolates a box of 2 * warp_buffer around the star (AU)
+        warp_thresh = -14   # log of density threshold for which we can see the warp in the primary
+        warp_buffer = 500     # Isolates a box of 2 * warp_buffer around the star (AU)
         rho_c_warp, vx_c_warp, vy_c_warp, vz_c_warp, Lx_c_warp, Ly_c_warp, Lz_c_warp, warp_ids = isolate_disk(X_c, Y_c, Z_c, Px * au, Py * au, Pz * au, warp_buffer * au, rho_c, vx_c, vy_c, vz_c, Lx, Ly, Lz, warp_thresh) 
 
         # Find the radial extent of the warp
@@ -124,20 +125,20 @@ def main():
         XY_2D_plot(rho, X, Y, irad, itheta, title=rf'Density X-Y Plane $\theta = $ {itheta_deg}$^{{\circ}}$, t = {int(it * dt * ninterm / stoky)} kyr', colorbarlabel=r"$\log(\rho)$", savefig=True, figfolder=f'{fig_imgs}/dens_xy_theta{itheta}_rad{irad}_it{it}.png', showfig=False)
 
         # Plotting the warp densities 
-        # contours_3D(X_c/au, Y_c/au, Z_c/au, rho_c_warp, r_select, plot_args, colorbarlabel=r'$\rho [g/cm^3]$', title=rf'{sim_name} $\log(\rho)$ above $\rho = 10^{{{warp_thresh}}} g/cm^3$, t = {int(it * dt * ninterm / stoky)} kyr', savefig=True, figfolder=f'{fig_imgs}/warp_dens_thresh{warp_thresh}_it{it}.png', showfig=False)
+        contours_3D(X_c/au, Y_c/au, Z_c/au, rho_c_warp, r_select, plot_args, colorbarlabel=r'$\rho [g/cm^3]$', title=rf'{sim_name} $\log(\rho)$ above $\rho = 10^{{{warp_thresh}}} g/cm^3$, t = {int(it * dt * ninterm / stoky)} kyr', savefig=True, figfolder=f'{fig_imgs}/warp_dens_thresh{warp_thresh}_it{it}.png', showfig=False)
 
         # Plotting the Cartesian warp angular momenta
         # quiver_plot_3d(X_c/au, Y_c/au, Z_c/au, Lx_c_warp, Ly_c_warp, Lz_c_warp, stagger=100, length=3, title="Warp Angular Momenta", colorbarlabel="logL", savefig=False, figfolder=f'{fig_imgs}/warp_L_thresh{warp_thresh}_it{it}.png', logmag=True, ignorecol=True, showfig=False)
 
         # Calculating the radial profile of warp inclination and twist
-        Lx_warp_avg, Ly_warp_avg, Lz_warp_avg = calc_L_average(Lx_c_warp, Ly_c_warp, Lz_c_warp)
+        Lx_warp_avg, Ly_warp_avg, Lz_warp_avg = calc_L_average(Lx_c_warp, Ly_c_warp, Lz_c_warp, mass)
         inc, twist = calc_inc_twist(Lx_warp_avg, Ly_warp_avg, Lz_warp_avg, domains["r"], savefig=False, plot=False)
         inc_it.append(inc)
         prec_it.append(twist)
 
         # Calculating and plotting the total angular momentum of the warped disk
-        # Lx_disk, Ly_disk, Lz_disk = calc_total_L(Lx_warp_avg, Ly_warp_avg, Lz_warp_avg)
-        # L_prim_mag = np.sqrt(Lx_disk**2 + Ly_disk**2 + Lz_disk**2)
+        Lx_disk, Ly_disk, Lz_disk = calc_total_L(Lx_warp_avg, Ly_warp_avg, Lz_warp_avg)
+        L_prim_mag = np.sqrt(Lx_disk**2 + Ly_disk**2 + Lz_disk**2)
 
         # Calculating and plotting the radial profile of warp precession as a quiver plot
         plot_twist_arrows(Lx_warp_avg, Ly_warp_avg, Lz_warp_avg, domains["r"], r_select, plot_args, title=f"Warp twist {sim_name} t={int(calc_simtime(it))} kyr", savefig=True, figfolder=f'{fig_imgs}/warp_twist_arrows_it{it}.png', showfig=False)
@@ -314,7 +315,7 @@ def main():
 
 
     # Make a time evolution GIF out of the 3D surface density and twist plots
-    # make_evol_GIF(fig_imgs, "warp_dens_thresh", "warp_dens_movie")
+    make_evol_GIF(fig_imgs, "warp_dens_thresh", "warp_dens_movie")
     make_evol_GIF(fig_imgs, "warp_twist_arrows", "warp_twist_movie")
     make_evol_GIF(fig_imgs, "dens_cyl_phi", "dens_cyl_movie")
     make_evol_GIF(fig_imgs, "dens_xy_theta", "dens_xy_movie")
