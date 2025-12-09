756b00c17e5a2f1a9458e960184f5e046b4f6ae4
drkndl <drishikanadella@gmail.com>
2025-12-09 11:32:19 +0100

    param study pro vs retro plots

--- param_study.py
+++ param_study.py
@@ -26,8 +26,8 @@ plt.rcParams['ytick.labelsize'] = 12     # y-tick label size
 plt.rcParams['legend.fontsize'] = 11     # legend font size
 
 # colormaps = load_sciviscolor_colormaps("discrete-5-4-section-blue-orange.xml")
-colours = cmaps.drought_severity.discrete(4)
-colours = colours(np.linspace(0, 1, 4))
+colours = cmaps.drought_severity.discrete(2)
+colours = colours(np.linspace(0, 1, 2))
 
 
 def main():
@@ -36,17 +36,23 @@ def main():
     # folders = [Path("../fargo3d/outputs/cloud_disk_it450_rotX45"), Path("../fargo3d/outputs/cloud_disk_it450_rotY45"), Path("../fargo3d/outputs/cloud_disk_it450_Rout30_rotX45"), Path("../fargo3d/outputs/cloud_disk_it450_Rout30_rotY45"), Path("../fargo3d/outputs/cloud_disk_it450_cmass10_rotX45"), Path("../fargo3d/outputs/cloud_disk_it450_cmass10_rotY45"), Path("../fargo3d/outputs/cloud_disk_it450_cmass10_Rout30_rotX45"), Path("../fargo3d/outputs/cloud_disk_it450_cmass10_Rout30_rotY45")]
 
     # Simulation data locally
-    folders = [Path("../cloud_disk_it450_rotX45"), Path("../cloud_disk_it450_rotY45"), Path("../cloud_disk_it450_Rout30_rotX45"), Path("../cloud_disk_it450_Rout30_rotY45"), Path("../cloud_disk_it450_cmass10_rotX45"), Path("../cloud_disk_it450_cmass10_rotY45"), Path("../cloud_disk_it450_cmass10_Rout30_rotX45"), Path("../cloud_disk_it450_cmass10_Rout30_rotY45")] #, Path("../cloud_disk_it450_retro_rotX45"), Path("../cloud_disk_it450_retro_rotY45")]
+    # folders = [Path("../cloud_disk_it450_rotX45"), Path("../cloud_disk_it450_rotY45"), Path("../cloud_disk_it450_Rout30_rotX45"), Path("../cloud_disk_it450_Rout30_rotY45"), Path("../cloud_disk_it450_cmass10_rotX45"), Path("../cloud_disk_it450_cmass10_rotY45"), Path("../cloud_disk_it450_cmass10_Rout30_rotX45"), Path("../cloud_disk_it450_cmass10_Rout30_rotY45")] #, Path("../cloud_disk_it450_retro_rotX45"), Path("../cloud_disk_it450_retro_rotY45")]
 
-    folders_labels = {"cloud_disk_it450_rotX45": r"$\mathrm{X_{45}, M_{c} / M_{d}=0.45, R_{out} = 100}$", "cloud_disk_it450_rotY45": r"$\mathrm{Y_{45}, M_{c} / M_{d}=0.45, R_{out} = 100}$", 
-    "cloud_disk_it450_Rout30_rotX45": r"$\mathrm{X_{45}, M_{c} / M_{d}=0.45, R_{out} = 30}$",
-    "cloud_disk_it450_Rout30_rotY45": r"$\mathrm{Y_{45}, M_{c} / M_{d}=0.45, R_{out} = 30}$",
-    "cloud_disk_it450_cmass10_rotX45": r"$\mathrm{X_{45}, M_{c} / M_{d}=4.5, R_{out} = 100}$",
-    "cloud_disk_it450_cmass10_rotY45": r"$\mathrm{Y_{45}, M_{c} / M_{d}=4.5, R_{out} = 100}$",
-    "cloud_disk_it450_cmass10_Rout30_rotX45": r"$\mathrm{X_{45}, M_{c} / M_{d}=4.5, R_{out} = 30}$",
-    "cloud_disk_it450_cmass10_Rout30_rotY45": r"$\mathrm{Y_{45}, M_{c} / M_{d}=4.5, R_{out} = 30}$"} #,
+    # Simulation data locally (prograde vs retrograde comparison)
+    folders = [Path("../cloud_disk_it450_rotX45"), Path("../cloud_disk_it450_rotY45"), Path("../cloud_disk_it450_retro_rotX45"), Path("../cloud_disk_it450_retro_rotY45")]
+
+    # folders_labels = {"cloud_disk_it450_rotX45": r"$\mathrm{X_{45}, M_{c} / M_{d}=0.45, R_{out} = 100}$", "cloud_disk_it450_rotY45": r"$\mathrm{Y_{45}, M_{c} / M_{d}=0.45, R_{out} = 100}$", 
+    # "cloud_disk_it450_Rout30_rotX45": r"$\mathrm{X_{45}, M_{c} / M_{d}=0.45, R_{out} = 30}$",
+    # "cloud_disk_it450_Rout30_rotY45": r"$\mathrm{Y_{45}, M_{c} / M_{d}=0.45, R_{out} = 30}$",
+    # "cloud_disk_it450_cmass10_rotX45": r"$\mathrm{X_{45}, M_{c} / M_{d}=4.5, R_{out} = 100}$",
+    # "cloud_disk_it450_cmass10_rotY45": r"$\mathrm{Y_{45}, M_{c} / M_{d}=4.5, R_{out} = 100}$",
+    # "cloud_disk_it450_cmass10_Rout30_rotX45": r"$\mathrm{X_{45}, M_{c} / M_{d}=4.5, R_{out} = 30}$",
+    # "cloud_disk_it450_cmass10_Rout30_rotY45": r"$\mathrm{Y_{45}, M_{c} / M_{d}=4.5, R_{out} = 30}$"} #,
     # "cloud_disk_it450_retro_rotX45": r"$\mathrm{X_{45, retro}, M_{c} / M_{d}=0.45, R_{out} = 100}$",
     # "cloud_disk_it450_retro_rotY45": r"$\mathrm{Y_{45, retro}, M_{c} / M_{d}=0.45, R_{out} = 100}$"}
+
+    folders_labels = {"cloud_disk_it450_rotX45": r"$\mathrm{X_{45, pro}, M_{c} / M_{d}=0.45, R_{out} = 100}$", "cloud_disk_it450_rotY45": r"$\mathrm{Y_{45, pro}, M_{c} / M_{d}=0.45, R_{out} = 100}$", "cloud_disk_it450_retro_rotX45": r"$\mathrm{X_{45, retro}, M_{c} / M_{d}=0.45, R_{out} = 100}$",
+    "cloud_disk_it450_retro_rotY45": r"$\mathrm{Y_{45, retro}, M_{c} / M_{d}=0.45, R_{out} = 100}$"}
 
     disk_mass_folder = {}              # Disk masses m(t, theta, r, phi) for all sims
     disk_inc_avg_folder = {}           # Average disk inclination inc(t) for all sims
@@ -187,11 +193,11 @@ def main():
 
     # Plotting inc_avg vs time 
     fig, ax = plt.subplots(figsize=(11, 6))
-    param_study_plot(fig, ax, disk_inc_avg_folder, allit_years, folders_labels, colours, xlabel=r"Time [kyr]", ylabel=r"$\mathrm{inc_{avg} [deg]}$", title=fr"Time Evolution of Average Inclinations $(\mathrm{{\rho \geq 10^{warp_thresh}}})$", figfolder=f'param_study_inc_avg_vs_t_warp{warp_thresh}.png', savefig=True, showfig=True)
+    param_study_plot(fig, ax, disk_inc_avg_folder, allit_years, folders_labels, colours, xlabel=r"Time [kyr]", ylabel=r"$\mathrm{inc_{avg} [deg]}$", title=fr"Time Evolution of Average Inclinations $(\mathrm{{\rho \geq 10^{warp_thresh}}})$", figfolder=f'pro_vs_retro_inc_avg_vs_t_warp{warp_thresh}.png', savefig=True, showfig=True)
 
     # Plotting twist_avg vs time 
     fig, ax = plt.subplots(figsize=(11, 6))
-    param_study_plot(fig, ax, disk_twist_avg_folder, allit_years, folders_labels, colours, xlabel=r"Time [kyr]", ylabel=r"$\mathrm{twist_{avg} [deg]}$", title=fr"Time Evolution of Average Twist $(\mathrm{{\rho \geq 10^{warp_thresh}}})$", figfolder=f'param_study_twist_avg_vs_t_warp{warp_thresh}.png', savefig=True, showfig=True)
+    param_study_plot(fig, ax, disk_twist_avg_folder, allit_years, folders_labels, colours, xlabel=r"Time [kyr]", ylabel=r"$\mathrm{twist_{avg} [deg]}$", title=fr"Time Evolution of Average Twist $(\mathrm{{\rho \geq 10^{warp_thresh}}})$", figfolder=f'pro_vs_retro_twist_avg_vs_t_warp{warp_thresh}.png', savefig=True, showfig=True)
 
     # Plotting absolute values of twist_avg vs time 
     fig, ax = plt.subplots(figsize=(11, 6))
@@ -219,7 +225,7 @@ def main():
     ax.set_title(fr"Time Evolution of Absolute Values of Average Twist $(\mathrm{{\rho \geq 10^{warp_thresh}}})$")
     ax.legend(loc='upper left', bbox_to_anchor=(1.05, 1), borderaxespad=0.)   # loc='upper left',
     plt.tight_layout() 
-    plt.savefig(f'param_study_absolute_twist_avg_vs_t_warp{warp_thresh}.png')
+    plt.savefig(f'pro_vs_retro_absolute_twist_avg_vs_t_warp{warp_thresh}.png')
     plt.show()
 
     # # Plotting time evolution of cumulative mass at 100 AU
@@ -248,7 +254,7 @@ def main():
     # ax.set_title(fr"Time Evolution of $\mathrm{{\log(M_{{cum}}(r))}}$ (100 AU)")
     # ax.legend(loc='upper left', bbox_to_anchor=(1.05, 1), borderaxespad=0.)   # loc='upper left', 
     # plt.tight_layout()
-    # plt.savefig('param_study_Mcumsum_100AU_vs_t.png')
+    # plt.savefig('pro_vs_retro_Mcumsum_100AU_vs_t.png')
     # plt.show()
 
     # # Plotting time evolution of dMcumdlogr at 100 AU
@@ -277,7 +283,7 @@ def main():
     # ax.set_title(fr"Time Evolution of $\mathrm{{\log(dM_{{cum}}(r)/d\log(r))}}$ (100 AU)")
     # ax.legend(loc='upper left', bbox_to_anchor=(1.05, 1), borderaxespad=0.)   # loc='upper left', 
     # plt.tight_layout()
-    # plt.savefig('param_study_dMcumdlogr_100AU_vs_t.png')
+    # plt.savefig('pro_vs_retro_dMcumdlogr_100AU_vs_t.png')
     # plt.show()
 
     # Plotting the mass accretion rate onto star vs time
@@ -306,7 +312,7 @@ def main():
     ax.set_title(fr"Time Evolution of Mass Accretion Rate onto Star $(\mathrm{{\rho \geq 10^{warp_thresh}}})$")
     ax.legend(loc='upper left', bbox_to_anchor=(1.05, 1), borderaxespad=0.)   # loc='upper left', 
     plt.tight_layout()
-    plt.savefig(f'param_study_Mdot_vs_t_warp{warp_thresh}.png')
+    plt.savefig(f'pro_vs_retro_Mdot_vs_t_warp{warp_thresh}.png')
     plt.show()
 
 
@@ -339,7 +345,7 @@ def main():
     ax.set_title(fr"Disk Inclination vs logr (53 kyr) $(\mathrm{{\rho \geq 10^{{{warp_thresh}}}}})$")
     ax.legend(loc='upper left', bbox_to_anchor=(1.05, 1), borderaxespad=0.)   # loc='upper left', 
     plt.tight_layout()
-    plt.savefig(f'param_study_inc_final_iter_vs_r_warp{warp_thresh}.png')
+    plt.savefig(f'pro_vs_retro_inc_final_iter_vs_r_warp{warp_thresh}.png')
     plt.show()
 
     # Plotting twist_final vs R
@@ -368,7 +374,7 @@ def main():
     ax.set_title(fr"Twist vs logr (53 kyr) $(\mathrm{{\rho \geq 10^{warp_thresh}}})$")
     ax.legend(loc='upper left', bbox_to_anchor=(1.05, 1), borderaxespad=0.)   # loc='upper left', 
     plt.tight_layout()
-    plt.savefig(f'param_study_twist_final_iter_vs_r_warp{warp_thresh}.png')
+    plt.savefig(f'pro_vs_retro_twist_final_iter_vs_r_warp{warp_thresh}.png')
     plt.show()
 
     # Plotting Mcumsum_final vs R
@@ -397,7 +403,7 @@ def main():
     ax.set_title(fr"$\mathrm{{\log(M_{{cum}}(r))}}$ vs logr (53 kyr) $(\mathrm{{\rho \geq 10^{warp_thresh}}})$")
     ax.legend(loc='upper left', bbox_to_anchor=(1.05, 1), borderaxespad=0.)   # loc='upper left', 
     plt.tight_layout()
-    plt.savefig(f'param_study_Mcumsum_final_iter_vs_r_warp{warp_thresh}.png')
+    plt.savefig(f'pro_vs_retro_Mcumsum_final_iter_vs_r_warp{warp_thresh}.png')
     plt.show()
 
     # Plotting dMcumdlogr_final vs R
@@ -426,7 +432,7 @@ def main():
     ax.set_title(fr"$\mathrm{{\log(dM_{{cum}}(r)/d\log(r))}}$ vs logr (53 kyr) $(\mathrm{{\rho \geq 10^{warp_thresh}}})$")
     ax.legend(loc='upper left', bbox_to_anchor=(1.05, 1), borderaxespad=0.)   # loc='upper left', 
     plt.tight_layout()
-    plt.savefig(f'param_study_dMcumdlogr_final_iter_vs_r_warp{warp_thresh}.png')
+    plt.savefig(f'pro_vs_retro_dMcumdlogr_final_iter_vs_r_warp{warp_thresh}.png')
     plt.show()
 
     # Making a GIF to show time evolution of dMcumdlogr vs logR
@@ -457,7 +463,7 @@ def main():
     #     ax.set_ylim(26, 33)
     #     ax.legend(loc='upper left', bbox_to_anchor=(1.05, 1), borderaxespad=0.)   # loc='upper left', 
     #     plt.tight_layout()
-    #     plt.savefig(f'param_study_dMcumdlogr_it{t}.png')
+    #     plt.savefig(f'pro_vs_retro_dMcumdlogr_it{t}.png')
     #     plt.close()
 
     # make_evol_GIF(".", "param_study_dMcumdlogr_it", f"param_study_dMcumdlogr_warp{warp_thresh}_movie")
