b2c7cf63e8887d07b33453a80db343bbb1504086
tr323@uni-heidelberg.de <hd_tr323@login02.cluster>
2025-12-12 09:54:17 +0100

    cover phi wedge for all iters

--- cover_phi_wedge.py
+++ cover_phi_wedge.py
@@ -58,30 +58,35 @@ def main():
 
     # folder = Path("../cloud_disk_it450_retro_rotX45/")                            # Folder with the output files
     folder = Path("../fargo3d/outputs/cloud_disk_it450_retro_rotX45")               # Folder with the output files (BinAC2)
-    ext_folder = Path("../fargo3d/outputs/cloud_disk_it450_retro_rotX45_vtk/")      # Folder to save images
-    it = 450                                                                  # FARGO snapshot of interest
+    ext_folder = Path("../fargo3d/outputs/cloud_disk_it450_retro_rotX45_vtk/")      # Folder to save new dat and vtk files
+    it = 450                                                                        # FARGO total snapshots
+    N = 5                                                                           # Save vtk files for every N files 
+
     domains = get_domain_spherical(folder)
-    rho = get_data(folder, "dens", it, domains)         # Load 3D array of density values
-    vphi = get_data(folder, "vx", it, domains)          # Load 3D array of azimuthal velocities v_phi
-    vrad = get_data(folder, "vy", it, domains)          # Load 3D array of radial velocities v_rad
-    vthe = get_data(folder, "vz", it, domains)          # Load 3D array of colatitude velocities v_theta
-    energy = get_data(folder, "energy", it, domains)    # Load 3D array of energies 
 
-    #############################   Make azimuthal pizza full #########################################
+    for i in range(0, it+1, N):
 
-    rho_ext, phi_ext = cover_phi_wedge(rho, domains["theta"], domains["r"], domains["phi"])      # Cover empty wedge in dens
-    vphi_ext, phi_ext = cover_phi_wedge(vphi, domains["theta"], domains["r"], domains["phi"])    # Cover empty wedge in vx
-    vrad_ext, phi_ext = cover_phi_wedge(vrad, domains["theta"], domains["r"], domains["phi"])    # Cover empty wedge in vy
-    vthe_ext, phi_ext = cover_phi_wedge(vthe, domains["theta"], domains["r"], domains["phi"])    # Cover empty wedge in vz 
-    E_ext, phi_ext = cover_phi_wedge(energy, domains["theta"], domains["r"], domains["phi"])     # Cover empty wedge in energy 
+        rho = get_data(folder, "dens", i, domains)         # Load 3D array of density values
+        vphi = get_data(folder, "vx", i, domains)          # Load 3D array of azimuthal velocities v_phi
+        vrad = get_data(folder, "vy", i, domains)          # Load 3D array of radial velocities v_rad
+        vthe = get_data(folder, "vz", i, domains)          # Load 3D array of colatitude velocities v_theta
+        energy = get_data(folder, "energy", i, domains)    # Load 3D array of energies 
 
-    #################################### Write new data ################################################
+        #############################   Make azimuthal pizza full #########################################
 
-    write_dat(ext_folder, "dens", it, rho_ext)       # Write 3D array of densities into dat file (nth, nr, nphi+2)
-    write_dat(ext_folder, "vx", it, vphi_ext)        # Write 3D array of vx into dat file (nth, nr, nphi+2)
-    write_dat(ext_folder, "vy", it, vrad_ext)        # Write 3D array of vy into dat file (nth, nr, nphi+2)
-    write_dat(ext_folder, "vz", it, vthe_ext)        # Write 3D array of vz into dat file (nth, nr, nphi+2)
-    write_dat(ext_folder, "energy", it, E_ext)       # Write 3D array of energy into dat file (nth, nr, nphi+2)
+        rho_ext, phi_ext = cover_phi_wedge(rho, domains["theta"], domains["r"], domains["phi"])      # Cover empty wedge in dens
+        vphi_ext, phi_ext = cover_phi_wedge(vphi, domains["theta"], domains["r"], domains["phi"])    # Cover empty wedge in vx
+        vrad_ext, phi_ext = cover_phi_wedge(vrad, domains["theta"], domains["r"], domains["phi"])    # Cover empty wedge in vy
+        vthe_ext, phi_ext = cover_phi_wedge(vthe, domains["theta"], domains["r"], domains["phi"])    # Cover empty wedge in vz 
+        E_ext, phi_ext = cover_phi_wedge(energy, domains["theta"], domains["r"], domains["phi"])     # Cover empty wedge in energy 
+
+        #################################### Write new data ################################################
+
+        write_dat(ext_folder, "dens", i, rho_ext)       # Write 3D array of densities into dat file (nth, nr, nphi+2)
+        write_dat(ext_folder, "vx", i, vphi_ext)        # Write 3D array of vx into dat file (nth, nr, nphi+2)
+        write_dat(ext_folder, "vy", i, vrad_ext)        # Write 3D array of vy into dat file (nth, nr, nphi+2)
+        write_dat(ext_folder, "vz", i, vthe_ext)        # Write 3D array of vz into dat file (nth, nr, nphi+2)
+        write_dat(ext_folder, "energy", i, E_ext)       # Write 3D array of energy into dat file (nth, nr, nphi+2)
     
 
 if __name__ == "__main__":
